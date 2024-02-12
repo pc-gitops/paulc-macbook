86fefb6ef24d825331ec5c39f37a4e71dd631afb
Paul Carlton <pcarlton@contractor.nabancard.com>
2024-02-12 11:37:39 +0000

    test workflow

--- infra-runner/bin/build-image.sh
+++ infra-runner/bin/build-image.sh
@@ -4,6 +4,13 @@
 
 set -xe
 
+version=${1:-}
+
+if [ -z "$version" ]; then
+    echo "Usage: $0 <image version>"
+    exit
+fi
+
 export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 export BASE_DIR=$(git rev-parse --show-toplevel)
 
@@ -12,7 +19,7 @@ pushd ${BASE_DIR}/infra-runner
 export INFRA_EXEC_VERSION=1.5.7
 export INFRA_EXEC_NAME=terraform
 export INFRA_EXEC_URL_PREFIX=https://releases.hashicorp.com/terraform/
-export VERSION=0.0.3
+export VERSION=0.0.$version
 
 docker build . -f Dockerfile --no-cache \
     --build-arg="INFRA_EXEC_VERSION=$INFRA_EXEC_VERSION" \
